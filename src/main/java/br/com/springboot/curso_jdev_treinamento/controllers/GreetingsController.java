86529fdc84883793a43fa6d8efe08ad350f159dd
Henrique Alvaro <henrique.coolcat15@gmail.com>
2023-01-15 18:14:17 -0300

    funcionalidade de salvar
    funcao de salvar pela api no db

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -54,6 +56,15 @@ public class GreetingsController {
     	List<Usuario> usuarios = usuarioRepository.findAll();/*excuta a consulta no banco de dados*/
     	
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/*Retorna a lista em json*/
+    }
+    
+    @PostMapping(value = "salvar")
+    @ResponseBody/*Descriçao da resposta*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){/*Recebe os dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
     
     
