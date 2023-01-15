63252324a8228a3706d4543d0c2f71d6e845bf90
Henrique Alvaro <henrique.coolcat15@gmail.com>
2023-01-15 00:50:42 -0300

    lista dos usuarios
    funcionalidade de listar usuario

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,11 +1,20 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 /**
  *
@@ -13,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
     /**
      *
      * @param name the name to greet
@@ -24,10 +36,24 @@ public class GreetingsController {
         return "Curso Spring Boot API " + name + "!";
     }
     
-    @RequestMapping(value = "/OlaMundo/{nome}", method = RequestMethod.GET)
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String metodoOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario);/*Grava no banco de dados*/
     	return "Ola mundo "+ nome;
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();/*excuta a consulta no banco de dados*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/*Retorna a lista em json*/
     }
     
     
